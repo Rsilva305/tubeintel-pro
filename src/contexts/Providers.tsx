7f4e9079748ffeefa381981f0006a882556febc9
ruben <silva.ruben@gmail.coim>
2025-05-06 10:41:27 -0400

    Add ThemeContext and update Providers for dark mode support

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -2,11 +2,14 @@
 
 import { ReactNode } from 'react';
 import { AuthProvider } from './AuthContext';
+import { ThemeProvider } from './ThemeContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
-      {children}
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
     </AuthProvider>
   );
 } 
